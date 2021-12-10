c997534673cfdb457451f1e6892efdf26ca66601
Fernando Ancajima <fernando.ancajima99@gmail.com>
2021-12-10 17:26:25 -0500

    probando vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
 
-  baseUrl: 'https://back-mean-m8y49gs6x-fernandoanc.vercel.app/music'
+  // baseUrl: 'https://back-mean.vercel.app/music',
+  baseUrl: 'https://back-mean-h56485tiz-fernandoanc.vercel.app/music'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,9 @@
 export const environment = {
   production: false,
 
-  baseUrl: 'http://localhost:4000/music'
+  baseUrl: 'http://localhost:4000/music',
+  // baseUrl: 'https://back-mean.vercel.app/music',
+  // baseUrl: 'https://back-mean-h56485tiz-fernandoanc.vercel.app/music',
 };
 
 /*
