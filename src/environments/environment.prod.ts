9d95f55380910f4246580426343b84d4141b4152
Fernando Ancajima <fernando.ancajima99@gmail.com>
2021-12-15 13:11:22 -0500

    change back to git hub

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
 
-  baseUrl: 'https://template-back-mean.herokuapp.com/music'
+  // baseUrl: 'https://template-back-mean.herokuapp.com/music',
+  baseUrl: 'https://template-angular-mean.herokuapp.com/music',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,7 @@ export const environment = {
   production: false,
 
   baseUrl: 'http://localhost:4000/music',
-  // baseUrl: 'https://back-mean.vercel.app/music',
-  // baseUrl: 'https://back-mean-h56485tiz-fernandoanc.vercel.app/music',
+  // baseUrl: 'https://template-angular-mean.herokuapp.com/music',
 };
 
 /*
