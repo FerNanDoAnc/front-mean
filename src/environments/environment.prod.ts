cfcae053fdb0e1a2e1c7ce255a07b50c1592bf35
Fernando Ancajima <fernando.ancajima99@gmail.com>
2021-12-15 10:21:34 -0500

    enviroment prod update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
 
-  // baseUrl: 'https://back-mean.vercel.app/music',
-  baseUrl: 'https://back-mean-h56485tiz-fernandoanc.vercel.app/music'
+  baseUrl: 'https://template-back-mean.herokuapp.com/music'
 };
